d85b0debc3e555e429499e9edb91af241d2762fb
MercedesAAlvarez <mechychevy@gmail.com>
2022-06-23 17:33:28 -0300

    arreglado problema con imagenes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require ('path')
 const app = express ()
 const port = 3001
 
-app.use(express.static(path.join(__dirname,'puplic')))
+app.use(express.static(path.resolve(__dirname,'public')))
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','index.html')))
 
 app.listen(port,() => console.log(`El servidor fue levantado con exito en http://localhost:${port}`))
