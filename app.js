649395d760808dea1f723afe173dc71fbc803b5e
MercedesAAlvarez <mechychevy@gmail.com>
2022-06-24 09:59:31 -0300

    solucionado problema de node, cambie imagen del logo

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require ('express')
 const path = require ('path')
 const app = express ()
-const port = 3001
+const port = 3030
 
 app.use(express.static(path.resolve(__dirname,'public')))
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','index.html')))
